ae98f74a5e86ba10b63e97a411d030baac4e63bc
Yakov Manuilov <110241091+IooEWREYooI@users.noreply.github.com>
2022-08-21 19:54:01 +0300

    Update BotsApplication.java

--- Bots/src/main/java/Telegram/Bots/BotsApplication.java
+++ Bots/src/main/java/Telegram/Bots/BotsApplication.java
@@ -7,8 +7,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import config.SQLraidtable;
-import service.RaidTableBotCommands;
 import service.u_bablo_botCommands;
 
 @SpringBootApplication
@@ -19,29 +17,6 @@ public class BotsApplication {
 		
 		// Указания места JDBC для Spring //
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		// Получение таблицы raidtable //
-		try {
-			SQLraidtable.getAllTableSortByPoints("\n\nИмя - @", " , количество баллов - ");
-		} catch (SQLException e) {
-			 System.out.println("С таблицей raidtable возникла ошибка ->");
-			 e.printStackTrace();
-		}
-		System.out.println("Таблица raidtable создана и отсортирована по возврастанию баллов");
-		//////////////////////////////////////////////////////////////////////////////////////
-		
-		////// Запуск бота RaidTableBot //////
-		RaidTableBotCommands bot0 = new RaidTableBotCommands();
-		try {
-			TelegramBotsApi RaidTableBot = new TelegramBotsApi(DefaultBotSession.class);
-			RaidTableBot.registerBot(bot0);
-		} catch (TelegramApiException e) {
-			System.out.println("Ошибка в "+bot0.getBotUsername());
-			e.printStackTrace();
-		}
-		System.out.println("Бот "+bot0.getBotUsername()+" успешно запущен");
-		////////////////////////////////////////////////////////
-		
 		
 		////// Запуск бота u_bablo_bot //////
 		u_bablo_botCommands bot1 = new u_bablo_botCommands();
